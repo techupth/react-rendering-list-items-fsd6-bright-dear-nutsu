42e627d4e7768cb4c3962578938a16c0d861a049
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2024-06-04 15:02:23 +0700

    feat(src/App.jsx): add movie list section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,54 @@
-import "./App.css";
+// import './App.css';
+
+import movies from './data/movies.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <section className="movie-list-section">
+    // <div className="App">
+    <div className="flex justify-center min-h-screen ">
+      {/* <section className="movie-list-section"> */}
+      <section className="flex flex-col gap-12 my-12 ">
+        <h1 className="text-center text-4xl font-bold">Movie List Section</h1>
         {/* Render Movie Lists Here */}
+        {movies.map((movie, index) => {
+          return (
+            <article
+              key={index}
+              className="w-[400px] p-3 border rounded-xl flex gap-3 font-semibold text-xl drop-shadow-3xl"
+            >
+              {/* drop-shadow-[0_4px_40px_rgba(145,124,124,0.25)] */}
+              <div>
+                <img
+                  className="w-[102px] h-[100px] rounded-lg"
+                  src={movie.image}
+                  alt="image-article"
+                />
+              </div>
+              <div>
+                <h2>Title: {movie.title}</h2>
+                <h3>Year: {movie.year}</h3>
+                <h3>Runtime: {movie.runtime}</h3>
+                <div className="flex gap-3">
+                  <h3 className="">Genres: </h3>
+                  <div className="flex flex-wrap gap-3">
+                    {movie.genres.map((genre, index) => {
+                      return (
+                        <button
+                          key={index}
+                          className="p-2 bg-red-300 rounded-2xl"
+                        >
+                          {genre}
+                        </button>
+                      );
+                    })}
+                  </div>
+                </div>
+                <h4>IMDB Rating: {movie.imdbRating}</h4>
+                <h4>IMDB Votes: {movie.imdbVotes}</h4>
+              </div>
+            </article>
+          );
+        })}
       </section>
     </div>
   );
